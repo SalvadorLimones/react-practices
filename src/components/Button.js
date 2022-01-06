e7540d782c1439b4054c80a2fd74839915e21e9e
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-01-06 16:14:05 +0100

    b

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,5 @@
 const buttonClick = (event) => document.querySelector(".App").insertAdjacentHTML('beforeend', `<div>One vote for ${event.target.innerHTML.split(" ").slice(-1)}</div>`);
 
-const Button = (props) => {
-   const name = props.userName;
-  return <button onClick={buttonClick}>Click here to vote for {name}!</button>;
-}  
+const Button = (props) => <button onClick={buttonClick}>Click here to vote for {props.userName}!</button>;  
 
 export default Button
