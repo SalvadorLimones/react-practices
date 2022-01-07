28ff3172ed14fa9ce1f0f39ec79bbf7d276b74a9
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-01-07 16:30:01 +0100

    add state practice

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import './App.css';
-import ButtonList from  './components/ButtonList';
+import ButtonWithClass from './components/ButtonWithClass';
+import ButtonWithHooks from './components/ButtonWithHooks';
 
 function App() {
   return (
     <div className="App">
-      <ButtonList/>
+      <ButtonWithClass/>
+      <ButtonWithHooks/>
     </div>
   );
 }
 
 export default App;
-
-//gomb clickre div-et ad hozzá egy szöveggel
-
-//változó stringgel és ezt eljuttatni a gomb feliratának
-
-//buttonlist komponens 10* jelenítse meg a gombot és ezt húzzuk be a komponensbe
